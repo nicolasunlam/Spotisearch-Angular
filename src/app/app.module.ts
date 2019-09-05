0cf7af9ad301aedaf9eaa17654429dcd99879566
nicolasunlam <nicolas.orlando24@gmail.com>
2019-09-05 12:39:15 -0300

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SboxComponent } from './components/sbox/sbox.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FavsComponent } from './components/favs/favs.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SboxComponent,
+    FooterComponent,
+    FavsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
