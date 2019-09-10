0f449532966dcd2d3ab735dfbd44df852f836c9e
nicolasunlam <nicolas.orlando24@gmail.com>
2019-09-10 14:21:26 -0300

    more fixes

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -11,18 +11,19 @@ export class ArtistComponent implements OnInit {
   
   private artists: [];
   constructor(private _Http: HttpClient) {
-    
-  }
+      
+   }
   
   ngOnInit() {
-    const id = '0TnOYISbd1XYRBk9myaseg';
-    this._Http.get('https://api.spotify.com/v1/artists/${id}/albums',{ headers: new HttpHeaders(
-    { 'Authorization: Bearer BQBAKOCtrOiOA2eY2CU7Er-4yBtb14cq5PkX1XO65ssEZnCxwLZZG5ku6hWLvOaUX2J_0ixURx5AVLlvNOeKWZSBaKoPS2fH4PgO7bRLUQFa3Mnyu66P5EFKHcISGsIwxH21r3Au-uccgN_H-Iee3ilfdmitnmSFzamFqFPLtgLaGV8'})
-  }).subscribe(({artist}: any) => {
-    debugger
-    this.artists = artist.items;
-    artists => console.log(artists);
-  }); 
-}
-}
+   const id = '0TnOYISbd1XYRBk9myaseg';
+    this._Http.get('´https://api.spotify.com/v1/artists/${id}/albums´',{ headers: new HttpHeaders(
+      { Authorization:'Bearer BQBAKOCtrOiOA2eY2CU7Er-4yBtb14cq5PkX1XO65ssEZnCxwLZZG5ku6hWLvOaUX2J_0ixURx5AVLlvNOeKWZSBaKoPS2fH4PgO7bRLUQFa3Mnyu66P5EFKHcISGsIwxH21r3Au-uccgN_H-Iee3ilfdmitnmSFzamFqFPLtgLaGV8'})
+    }).subscribe(({artist}: any) => {
+      debugger
+      this.artists = artist.items;
+      console.log(this.artists);
+    }); 
+    }
+  }
 
+// ´https://api.spotify.com/v1/artists/${id}/albums´
